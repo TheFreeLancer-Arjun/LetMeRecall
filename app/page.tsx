5a5eb1e9fe3335d5d0db22aa5e7b9f0ec3620a12
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-04-23 01:27:21 +0530

    preparing landing page's base

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
+import { Navbar } from "@/components/Navbar";
 import AuthInput from "@/components/ui/AuthInput";
+import { Button } from "@/components/ui/Button";
+import { EnterDoor } from "@/icons/EnterDoor";
+import { QuestionMark } from "@/icons/QuestionMark";
 
 export default function Home() {
     return (
         <div className="bg-mainBgColor min-h-screen">
-            <div className="flex justify-center">
-                <div className="mt-80">
+            <Navbar />
+            <div className="flex justify-around"> 
+                <div className="text-white bg-cover bg-cen">
+                    <div className="mt-5 text-xl md:text-6xl font-extrabold">
+                        Never Lose a Thought Again
+                    </div>
+                    <div className="text-xl">
+                        Your digital second brain for capturing, organizing, and rediscovering everything that matters.
+                    </div>
+
+                    <div>
+                        <Button variant="blue_variant" endIcon={<EnterDoor/>} text="Get Started"/>
+                        <Button variant="general_1" endIcon={<QuestionMark  />} text="See how it works"/>
+                    </div>
+                </div>
+                <div className="mt-60">
                     <AuthInput/>
                 </div>
             </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,28 @@
-export default function Navbar() {
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import GithubCorner from "./GithubCorner";
+
+export const Navbar = () => {
+    const router = useRouter();
+
     return (
-        <>
-            Navbar UnderProgress!
-        </>
-    )
-}
+        <div className="flex justify-center items-center p-2">
+            <GithubCorner/>
+            <div className="w-full max-w-screen-lg cursor-pointer h-36 border-2 mt-2 border-pink-300 rounded-2xl flex flex-col md:flex-row md:justify-center items-center shadow-sm shadow-blue-200 hover:shadow-lg hover:shadow-amber-200 transition-all duration-500 p-4">
+                {/* Logo */}
+                <div onClick={() => router.push("/")} className="flex justify-center">
+                    <Image
+                        src="/letmerecall-logo.png"
+                        alt="SWS logo"
+                        width={192}
+                        height={192}
+                        className="w-32 md:w-48"
+                        priority
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
